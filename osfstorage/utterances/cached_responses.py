0e9cf0f5f0d482fafe72c53090387d96a52616e0
PresQT <N/A>
2020-10-27 17:27:50 -0400

    PresQT Upload

---
+++ osfstorage/utterances/cached_responses.py
@@ -0,0 +1,17 @@
+help_response = "I am Bathroom Buddy. I exist for only two purposes.... \n\n" \
+    "`m`: I will tell you the current status of the men's bathroom on the programmer side. \n" \
+    "`w`: I will tell you the current status of the women's bathroom on the programmer side. \n\n" \
+    "As of now this is all I do, but if you would like me to learn something new please reach out to the Bathroom Buddy team."
+
+
+confused = [
+    "No one has taught me how to do that yet.",
+    "That seems like a bad idea...",
+    "What are you talking about?",
+    "What?",
+    "The `Natural Conversation` feature is only available in `BathroomBuddy Premium`. \n"
+    "To activate `BathroomBuddy Premium` please enter a valid CC number. \n"
+    "~(This information will not be stored securely.)~",
+    "Please keep questions limited to the status of the bathroom.",
+    "I'm not sure I can answer that..."
+]
