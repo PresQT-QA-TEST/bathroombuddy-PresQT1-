45b01ef5588dafc53f5b75ed5ca22ada0f237e98
PresQT <N/A>
2020-10-27 17:27:49 -0400

    PresQT Upload

---
+++ osfstorage/utterances/known_utterances.py
@@ -0,0 +1,70 @@
+known_utterances = [
+    "help",
+    "weather",
+    "uk time",
+    "mlb scores",
+    "nhl scores",
+    ":coronavirus:",
+    "m",
+    "M",
+    "w",
+    "W",
+    "chuck norris",
+    "advice",
+    "lyrics"
+]
+
+utterance_types = [
+    {
+        "utterance": "help",
+        "type": "help"
+    },
+    {
+        "utterance": "weather",
+        "type": "weather"
+    },
+    {
+        "utterance": "uk time",
+        "type": "uk time"
+    },
+    {
+        "utterance": "mlb scores",
+        "type": "mlb scores"
+    },
+    {
+        "utterance": "nhl scores",
+        "type": "nhl scores"
+    },
+    {
+        "utterance": ":coronavirus:",
+        "type": ":coronavirus:"
+    },
+    {
+        "utterance": "m",
+        "type": "m"
+    },
+    {
+        "utterance": "M",
+        "type": "m"
+    },
+    {
+        "utterance": "w",
+        "type": "w"
+    },
+    {
+        "utterance": "W",
+        "type": "w"
+    },
+    {
+        "utterance": "chuck norris",
+        "type": "chuck norris"
+    },
+    {
+        "utterance": "advice",
+        "type": "advice"
+    },
+    {
+        "utterance": "lyrics",
+        "type": "lyrics"
+    }
+]
