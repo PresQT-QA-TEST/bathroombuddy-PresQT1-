65dbb29a0d9e96d06015303c4aafdd6b3c09f6fc
PresQT <N/A>
2020-10-27 17:27:50 -0400

    PresQT Upload

---
+++ osfstorage/utterances/hidden_gems.py
@@ -0,0 +1,14 @@
+hidden_gems = {
+    "i love you": "I am incapable of returning that love. The bathroom is free though.",
+    "whats up": "Ask me about the bathroom.",
+    "dodge stratus": "The Dodge Stratus is a mid-sized car that was introduced by Dodge in 1995.",
+    "42": "That is a great answer!  Do you want to ask me a question?",
+    "easter egg": "\U0001F95A",
+    "hello there": "General Kenobi!",
+    "poozer": ":poozer:",
+    "is the bathroom free?": "mens or womens?",
+    "wassup": "waaaaaaaasssssssuuuupppppp",
+    "you're beautiful": ":blush: no u",
+    ":mattstairs:": "Matt Stairs is an absolute unit.",
+    "test": "Nothing to see here"
+}
